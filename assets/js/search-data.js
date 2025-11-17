0a349838edf94149ed287372d359f34ad75c991b
sydneyb24 <slbailey@smu.edu>
2025-11-17 00:14:42 +0000

    Deploying to gh-pages from @ sydneyb24/sydneyb24.github.io@52fcd8f372f0b5fbaec4d4f474e6e2eb51f2840c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,11 +481,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "projects-dds-project-1",
           title: 'DDS Project-1',
           description: "Frito Lay Attrition Project for Doing Data Science",
