105c7e8c3ce8318958cc4c4477583f0def54d58f
sydneyb24 <slbailey@smu.edu>
2025-11-17 00:32:29 +0000

    Deploying to gh-pages from @ sydneyb24/sydneyb24.github.io@ffc9ccc6126e47d47758bba6f767396025b069b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,12 +451,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
+          section: "News",},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
